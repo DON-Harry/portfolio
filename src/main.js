1922b4d6b1cae8a756adc4377dc01ec6b920a92d
Harsha0031 <harshaharry31@gmail.com>
2024-04-23 22:15:01 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import '../styles/modern-normalizer.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/about.css'
+import '../styles/components/featured.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/utils.css'
+
+import mobileNav from './utils/mobile-nav'
+import mode from './utils/dark-mode'
+mobileNav();
+mode();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
